81bdf2f2c994e2cfaaa0bc7e4fa94c34855e68f9
s-kella <52741545+s-kella@users.noreply.github.com>
2021-09-23 21:33:01 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,27 +13,26 @@ def main():
     chat_id = os.getenv('TG_CHAT_ID')
     header = {'Authorization': dvmn_token}
     bot = telegram.Bot(token=tg_token)
-    cur_time = time.time()
-    timestamp = cur_time
+    timestamp = time.time()
     while True:
         try:
             payload = {'timestamp': timestamp}
             response = requests.get(url, headers=header, timeout=4, params=payload)
             response.raise_for_status()
-            print(response.text)
-            if response.json()['status'] == 'found':
-                check_info = response.json()['new_attempts'][0]
-                timestamp = check_info['timestamp']
-                lesson_title = check_info['lesson_title']
-                lesson_url = f"dvmn.org{check_info['lesson_url']}"
-                if check_info['is_negative']:
+            response_data = response.json()
+            if response_data['status'] == 'found':
+                attempt_info = response_data['new_attempts'][0]
+                timestamp = response_data['last_attempt_timestamp']
+                lesson_title = attempt_info['lesson_title']
+                lesson_url = f"dvmn.org{attempt_info['lesson_url']}"
+                if attempt_info['is_negative']:
                     is_negative = 'К сожалению, в работе есть ошибки :('
                 else:
                     is_negative = 'Всё супер, можно приступать к слеующей задаче!'
                 bot.send_message(chat_id=chat_id,
                                  text=f'Преподаватель проверил вашу работу "{lesson_title}".\n\n{is_negative}\n\n{lesson_url}')
             else:
-                timestamp = response.json()['timestamp_to_request']
+                timestamp = response_data['timestamp_to_request']
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
